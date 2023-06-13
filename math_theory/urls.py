1cc4572b37245a5821f9413917bd433de571cb20
mark0g111 <9154547225@mail.ru>
2023-06-13 12:01:23 +0300

    urls in theory

--- math_theory/urls.py
+++ math_theory/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
 from theory import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('theory/', views.theory, name='theory'),
+    path('theory/', include('theory.urls')),
 ]
