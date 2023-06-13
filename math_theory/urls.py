550c1e46a4b0c5e3f19e4954a200bd4d7ba8b2e2
mark0g111 <9154547225@mail.ru>
2023-06-13 11:36:42 +0300

    theory page

--- math_theory/urls.py
+++ math_theory/urls.py
@@ -21,4 +21,5 @@ from theory import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('theory/', views.theory, name='theory'),
 ]
--- theory/views.py
+++ theory/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Theory
 
 
 def home(request):
     return render(request, 'theory/home.html')
+
+
+def theory(request):
+    theorys = Theory.objects.all()
+    return render(request, 'theory/theory.html', {'theorys': theorys})
