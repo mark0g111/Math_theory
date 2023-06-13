cdfdc6d533beca4b5343e6c3151082271c8788cf
mark0g111 <9154547225@mail.ru>
2023-06-13 12:10:32 +0300

    theory detail

--- theory/urls.py
+++ theory/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'theory'
+
 urlpatterns = [
     path('', views.theory, name='theory'),
+    path('<int:theory_id>', views.detail, name='detail'),
 ]
--- theory/views.py
+++ theory/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Theory
 
 
@@ -9,3 +10,7 @@ def home(request):
 def theory(request):
     theorys = Theory.objects.all()
     return render(request, 'theory/theory.html', {'theorys': theorys})
+
+def detail(request, theory_id):
+    theory = get_object_or_404(Theory, pk=theory_id)
+    return render(request, 'theory/detail.html', {'theory':theory})
