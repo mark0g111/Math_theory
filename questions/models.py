1b9ddcb2f9564213fb473de3b264538b45ea95fa
mark0g111 <9154547225@mail.ru>
2023-06-15 12:59:38 +0300

    add tasks

--- questions/models.py
+++ questions/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
--- theory/models.py
+++ theory/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Theory(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
