f611133bd5d90496acaea279efe8eccfe916ec28
mark0g111 <9154547225@mail.ru>
2023-06-15 13:37:35 +0300

    make task detail!

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Tasks(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.FileField(upload_to='tasks/files/', blank=True)
 
     def __str__(self):
         return self.title
